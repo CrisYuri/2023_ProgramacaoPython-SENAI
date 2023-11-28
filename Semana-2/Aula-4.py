cf813e75f52c2ed5ad8c5a0eb0473dd22402cdb2
Python <cristina.yuri@gmail.com>
2023-11-28 16:22:14 -0300

    Aula-4 Dicionario

--- Semana-2/Aula-4.py
+++ Semana-2/Aula-4.py
@@ -3,10 +3,8 @@
 frutas =[{'nome':'banana', 'cor': 'amarela', 'quantidade': 6},{'nome':'maçã', 'cor': 'vermelha', 'quantidade': 10}]
 tipo = {
     'nome': 'nome'
-    
+
 }
-for tipo in frutas:
-    print(frutas['nome'])
 
 print(frutas)
 print(frutas.items())
