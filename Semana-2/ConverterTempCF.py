b01ec4f235a5e9107a5f10d206b2909929edfecb
Python <cristina.yuri@gmail.com>
2023-11-29 16:35:41 -0300

    Aula-5 Dicionario 2

--- Semana-2/ConverterTempCF.py
+++ Semana-2/ConverterTempCF.py
@@ -1,3 +1,15 @@
+'''
+Objetivo:
+- Praticar o uso de funções;
+- Introduzir o uso de laços de repetição;
+- Compreender o uso do import para projetos de mais de um arquivo
+Proposta:
+- Criar um sistema de informação que use menu para converter ºC (graus celsius) em ºF (graus fahrenheit) e vice e versa;
+- Leve em conta as formulas dadas;
+- Use versionamento e colaboração e explore o Git e o github
+'''
+
+
 # C -> F = (0 °C × 9/5) + 32 = 32 °F
 # F -> C = (0 °F − 32) × 5/9 = -17,78 °C
 
