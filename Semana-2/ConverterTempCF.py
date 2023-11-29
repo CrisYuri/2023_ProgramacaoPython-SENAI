306fa9d51b80905480fcae963780f90c8223658c
Python <cristina.yuri@gmail.com>
2023-11-29 16:46:13 -0300

    Aula-5 Dicionario 2

--- Semana-2/ConverterTempCF.py
+++ Semana-2/ConverterTempCF.py
@@ -13,4 +13,13 @@ Proposta:
 # C -> F = (0 °C × 9/5) + 32 = 32 °F
 # F -> C = (0 °F − 32) × 5/9 = -17,78 °C
 
+temperatura = float(input('Temperatura: '))
+unidade = input('Unidade? (C ou F): ')
+
+if unidade == 'C':
+    tempConv = (temperatura * 9 / 5) + 32
+    print(f'{temperatura:.2f}C = {tempConv:.2f}F')
+elif unidade == 'F':
+    tempConv = (temperatura - 32) * 5 / 9
+    print(f'{temperatura:.2f}F = {tempConv:.2f}C')
 
