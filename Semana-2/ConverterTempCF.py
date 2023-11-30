9368f2d458aab4f054515e780efc688c075854ae
CrisYuri <cristina.yuri@gmail.com>
2023-11-30 10:52:47 -0300

    TempAtual

--- Semana-2/ConverterTempCF.py
+++ Semana-2/ConverterTempCF.py
@@ -7,11 +7,12 @@ Proposta:
 - Criar um sistema de informação que use menu para converter ºC (graus celsius) em ºF (graus fahrenheit) e vice e versa;
 - Leve em conta as formulas dadas;
 - Use versionamento e colaboração e explore o Git e o github
+
+C -> F = (0 °C × 9/5) + 32 = 32 °F
+F -> C = (0 °F − 32) × 5/9 = -17,78 °C
 '''
 
-
-# C -> F = (0 °C × 9/5) + 32 = 32 °F
-# F -> C = (0 °F − 32) × 5/9 = -17,78 °C
+import PySimpleGUI as sg
 
 temperatura = float(input('Temperatura: '))
 unidade = input('Unidade? (C ou F): ')
@@ -22,4 +23,3 @@ if unidade == 'C':
 elif unidade == 'F':
     tempConv = (temperatura - 32) * 5 / 9
     print(f'{temperatura:.2f}F = {tempConv:.2f}C')
-
