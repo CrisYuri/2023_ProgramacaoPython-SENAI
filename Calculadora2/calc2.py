1f9ced36aababf554494eac5e6c25f72cf6894da
CrisYuri <cristina.yuri@gmail.com>
2023-12-06 14:28:54 -0300

    Calculadora com menu Cris

--- Calculadora2/calc2.py
+++ Calculadora2/calc2.py
@@ -9,9 +9,13 @@ def sub(n1, n2):
 def mult(n1, n2):
     return n1 * n2
 
-print(mult(5, 3))
+def div(n1, n2):
+    return n1 / n2
+
 
 #  Teste
 # print(f'{2} + {2} = {soma(2, 2)}')
-# print(f'{2} + {2} = {sub(2, 2)}')
+# print(f'{2} - {2} = {sub(2, 2)}')
+# print(f'{2} * {2} = {mult(2, 2)}')
+# print(f'{2} / {2} = {div(2, 2)}')
 
--- Calculadora2/calculadora.py
+++ Calculadora2/calculadora.py
@@ -1,14 +1,70 @@
 #  Interface da calculadora
 
-from calc2 import soma
+from calc2 import*
 
-#  Apresentação
-print('\n\t\t\t -- Calculadora 2 --\n')
+while True:
+    # Apresentação
+    print('\n\t\t\t --- Calculadora Simples ---\n')
 
-#  Entrada
-num1 = int(input('Informe o n1: '))
-num2 = int(input('Informe o n2: '))
+    # Menu
+    print('1. Soma')
+    print('2. Subtração')
+    print('3. Multiplicação')
+    print('4. Divisão')
+    print('5. Sair')
 
-#  Processamento
-print(f'total = {soma(num1, num2)}')
+    op = int(input('\nOpção: '))
+
+    if op == 1:
+
+        # Entradas
+        n1 = int(input('Informe n1: '))
+        n2 = int(input('Informe n2: '))
+
+        # Processamento
+        total = soma(n1, n2)
+
+        # Saída
+        print(f'{n1} + {n2} = {total}')
+    
+    elif op == 2:
+        # Entradas
+        n1 = int(input('Informe n1: '))
+        n2 = int(input('Informe n2: '))
+
+        # Processamento
+        total = sub(n1, n2)
+
+        # Saída
+        print(f'{n1} - {n2} = {total}')
+    
+    elif op == 3:
+        # Entradas
+        n1 = int(input('Informe n1: '))
+        n2 = int(input('Informe n2: '))
+
+        # Processamento
+        total = mult(n1, n2)
+
+        # Saída
+        print(f'{n1} * {n2} = {total}')
+    
+    elif op == 4:
+        # Entradas
+        n1 = int(input('Informe n1: '))
+        n2 = int(input('Informe n2: '))
+
+        # Processamento
+        total = div(n1, n2)
+
+        # Saída
+        print(f'{n1} / {n2} = {total}')
+    
+    elif op == 5:
+        print('\nAté logo!\n')
+        break
+    
+    else:
+        print(f'Opção {op} incorreta!')
+
 
