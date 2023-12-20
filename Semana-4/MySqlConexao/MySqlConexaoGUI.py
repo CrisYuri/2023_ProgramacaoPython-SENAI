f985664a472ccd2c39e0b0404bae04c73018d0b5
CrisYuri <cristina.yuri@gmail.com>
2023-12-20 14:33:01 -0300

    mysql 3

--- Semana-4/MySqlConexao/MySqlConexaoGUI.py
+++ Semana-4/MySqlConexao/MySqlConexaoGUI.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as psg
-# import MySqlConexao as msqlc
+import MySqlConexao as msqlc
 
 layout = [[psg.Text('--- Lista de Estudantes Matriculados ---')],
           [psg.Text('Matrícula: '), psg.InputText(key='matricula')],
@@ -20,22 +20,22 @@ while True:
         janela['matricula'].update('')
         janela['nome'].update('')
         janela['lista'].update('')
-    # elif evento == 'Ver Lista':
-    #     janela['lista'].update(msqlc.read(msqlc.conectar()))
-    # elif evento == 'Cadastrar aluno':
-    #     m = valor['matricula']
-    #     n = valor['nome']
-    #     msqlc.create(msqlc.conectar(),[(m, n)])
-    #     janela['lista'].update(msqlc.read(msqlc.conectar()))
-    # elif evento == 'Remover aluno':
-    #     m = valor['matricula']
-    #     n = valor['nome']
-    #     msqlc.delete(msqlc.conectar(), [(m,n)])
-    #     janela['lista'].update(msqlc.read(msqlc.conectar()))
-    # elif evento == 'Editar aluno':
-    #     m = valor['matricula']
-    #     n = valor['nome']
-    #     msqlc.update(msqlc.conectar(),[(m, n)])
-    #     janela['lista'].update(msqlc.read(msqlc.conectar()))
+    elif evento == 'Ver Lista':
+        janela['lista'].update(msqlc.read(msqlc.conectar()))
+    elif evento == 'Cadastrar aluno':
+        m = valor['matricula']
+        n = valor['nome']
+        msqlc.create(msqlc.conectar(),[(m, n)])
+        janela['lista'].update(msqlc.read(msqlc.conectar()))
+    elif evento == 'Remover aluno':
+        m = valor['matricula']
+        n = valor['nome']
+        msqlc.delete(msqlc.conectar(), [(m,n)])
+        janela['lista'].update(msqlc.read(msqlc.conectar()))
+    elif evento == 'Editar aluno':
+        m = valor['matricula']
+        n = valor['nome']
+        msqlc.update(msqlc.conectar(),[(m, n)])
+        janela['lista'].update(msqlc.read(msqlc.conectar()))
 
 janela.close()
